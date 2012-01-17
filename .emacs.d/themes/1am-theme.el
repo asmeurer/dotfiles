f575f3340444b25f72943a19c7ebbecebd6afcc6
Aaron Meurer <asmeurer@gmail.com>
2012-01-17 13:57:54 -0700

    Correctly rename the theme from Midnight to 1am

--- .emacs.d/themes/1am-theme.el
+++ .emacs.d/themes/1am-theme.el
@@ -1,12 +1,11 @@
 (deftheme 1am
-  "Theme meant for green text on semi-transparent black
-  background. Loosely based on XCode's midnight theme (hence the name).")
+  "Theme meant for green text on semi-transparent black background. Loosely based on XCode's midnight theme (hence the name).")
 
 (custom-theme-set-faces
- 'Midnight
+ '1am
  '(highlight-indent-face ((t (:background "color-233"))))
  '(font-lock-string-face ((t (:foreground "red"))))
  '(font-lock-comment-face ((t (:foreground "white"))))
  '(font-lock-doc-face ((t (:inherit font-lock-string-face :foreground "brightyellow")))))
 
-(provide-theme 'Midnight)
+(provide-theme '1am)
