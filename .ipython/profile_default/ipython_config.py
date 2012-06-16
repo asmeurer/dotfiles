4835505015bc09dcb02e5aa934fc5bcfd6ca123e
Aaron Meurer <asmeurer@gmail.com>
2012-06-15 20:03:37 -0600

    Try the LightBG colors in IPython

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -137,10 +137,10 @@ c.TerminalIPythonApp.ignore_old_config = True
 #------------------------------------------------------------------------------
 
 # A Mixin for applications that start InteractiveShell instances.
-# 
+#
 # Provides configurables for loading extensions and executing files as part of
 # configuring a Shell environment.
-# 
+#
 # Provides init_extensions() and init_code() methods, to be called after
 # init_shell(), which must be implemented by subclasses.
 
@@ -176,28 +176,28 @@ c.TerminalInteractiveShell.autoedit_syntax = True
 # color codes, this capability can be turned off.
 # c.TerminalInteractiveShell.color_info = True
 
-# 
+#
 # c.TerminalInteractiveShell.history_length = 10000
 
-# 
+#
 # c.TerminalInteractiveShell.separate_in = '\n'
 
 # Set the color scheme (NoColor, Linux, or LightBG).
-c.TerminalInteractiveShell.colors = 'Linux'
+c.TerminalInteractiveShell.colors = 'LightBG'
 
 # Autoindent IPython code entered interactively.
 # c.TerminalInteractiveShell.autoindent = True
 
-# 
+#
 # c.TerminalInteractiveShell.readline_omit__names = 2
 
-# 
+#
 # c.TerminalInteractiveShell.prompt_in2 = '   .\\D.: '
 
-# 
+#
 # c.TerminalInteractiveShell.separate_out = ''
 
-# 
+#
 # c.TerminalInteractiveShell.prompt_in1 = 'In [\\#]: '
 
 # Enable deep (recursive) reloading by default. IPython can use the deep_reload
@@ -208,7 +208,7 @@ c.TerminalInteractiveShell.colors = 'Linux'
 # normal reload(), but deep_reload will still be available as dreload().
 # c.TerminalInteractiveShell.deep_reload = False
 
-# 
+#
 # c.TerminalInteractiveShell.debug = False
 
 # Make IPython automatically call any callable object even if you didn't type
@@ -232,13 +232,13 @@ c.TerminalInteractiveShell.autocall = 0
 # Set the editor used by IPython (default to $EDITOR/vi/notepad).
 # c.TerminalInteractiveShell.editor = 'edit -w --resume'
 
-# 
+#
 # c.TerminalInteractiveShell.prompts_pad_left = True
 
 # The part of the banner to be printed before the profile
 # c.TerminalInteractiveShell.banner1 = 'Python 2.7.2 (default, Jul 31 2011, 01:52:32) \nType "copyright", "credits" or "license" for more information.\n\nIPython 0.11 -- An enhanced Interactive Python.\n?         -> Introduction and overview of IPython\'s features.\n%quickref -> Quick reference.\nhelp      -> Python\'s own help system.\nobject?   -> Details about \'object\', use \'object??\' for extra details.\n'
 
-# 
+#
 c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete',
 '"\\C-l": clear-screen', 'set show-all-if-ambiguous on', '"\\C-o": tab-insert',
 '"\\C-r": reverse-search-history', '"\\C-s": forward-search-history',
@@ -250,13 +250,13 @@ c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete',
 # The part of the banner to be printed after the profile
 # c.TerminalInteractiveShell.banner2 = ''
 
-# 
+#
 # c.TerminalInteractiveShell.separate_out2 = ''
 
-# 
+#
 # c.TerminalInteractiveShell.wildcards_case_sensitive = True
 
-# 
+#
 # c.TerminalInteractiveShell.readline_merge_completions = True
 
 # Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
@@ -264,10 +264,10 @@ c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete',
 # direct exit without any confirmation.
 c.TerminalInteractiveShell.confirm_exit = False
 
-# 
+#
 # c.TerminalInteractiveShell.ipython_dir = ''
 
-# 
+#
 # c.TerminalInteractiveShell.readline_remove_delims = '-/~'
 
 # Start logging to the default log file.
@@ -282,25 +282,25 @@ c.TerminalInteractiveShell.confirm_exit = False
 # Enable magic commands to be called without the leading %.
 # c.TerminalInteractiveShell.automagic = True
 
-# 
+#
 # c.TerminalInteractiveShell.readline_use = True
 
 # Start logging to the given file in append mode.
 # c.TerminalInteractiveShell.logappend = ''
 
-# 
+#
 # c.TerminalInteractiveShell.xmode = 'Context'
 
-# 
+#
 # c.TerminalInteractiveShell.quiet = False
 
 # Enable auto setting the terminal title.
 # c.TerminalInteractiveShell.term_title = False
 
-# 
+#
 # c.TerminalInteractiveShell.object_info_string_level = 0
 
-# 
+#
 # c.TerminalInteractiveShell.prompt_out = 'Out[\\#]: '
 
 # Set the size of the output cache.  The default is 1000, you can change it
@@ -319,10 +319,10 @@ c.TerminalInteractiveShell.confirm_exit = False
 #------------------------------------------------------------------------------
 
 # An object to manage the profile directory and its resources.
-# 
+#
 # The profile directory is used by all IPython applications, to manage
 # configuration, logging and security.
-# 
+#
 # This object knows how to find, create and manage these directories. This
 # should be used by any code that wants to handle profiles.
 
@@ -335,12 +335,12 @@ c.TerminalInteractiveShell.confirm_exit = False
 #------------------------------------------------------------------------------
 
 # The default pretty-printer.
-# 
+#
 # This uses :mod:`IPython.external.pretty` to compute the format data of the
 # object. If the object cannot be pretty printed, :func:`repr` is used. See the
 # documentation of :mod:`IPython.external.pretty` for details on how to write
 # pretty printers.  Here is a simple example::
-# 
+#
 #     def dtype_pprinter(obj, p, cycle):
 #         if cycle:
 #             return p.text('dtype(...)')
@@ -358,26 +358,26 @@ c.TerminalInteractiveShell.confirm_exit = False
 
 # PlainTextFormatter will inherit config from: BaseFormatter
 
-# 
+#
 # c.PlainTextFormatter.type_printers = {}
 
-# 
+#
 # c.PlainTextFormatter.newline = '\n'
 
-# 
+#
 # c.PlainTextFormatter.float_precision = ''
 
-# 
+#
 # c.PlainTextFormatter.verbose = False
 
-# 
+#
 # c.PlainTextFormatter.deferred_printers = {}
 
-# 
+#
 # c.PlainTextFormatter.pprint = True
 
-# 
+#
 # c.PlainTextFormatter.max_width = 79
 
-# 
+#
 # c.PlainTextFormatter.singleton_printers = {}
