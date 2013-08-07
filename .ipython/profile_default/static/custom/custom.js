5be09c9e66957390ba3e71f0cbd99023f99befaa
Aaron Meurer <asmeurer@gmail.com>
2013-08-07 15:06:09 -0600

    Revert "Add emacs bindings to IPython notebook"
    This reverts commit 37ff4e27e284409802a3af18c89ab5c25f219103.
    
    They don't work.

--- .ipython/profile_default/static/custom/custom.js
+++
@@ -1,5 +0,0 @@
-// https://gist.github.com/minrk/5940801 (except with emacs, of course)
-$.getScript("/static/components/codemirror/keymap/emacs.js", function() {
-    if (! IPython.Cell) return;
-    IPython.Cell.options_default.cm_config.keyMap = "emacs";
-});
