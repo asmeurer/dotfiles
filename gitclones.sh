60448ce19c7895bb50bc4daf3a971b80447a4ce6
Aaron Meurer <asmeurer@gmail.com>
2014-01-13 13:11:17 -0600

    Add IPython to the git clones

--- gitclones.sh
+++ gitclones.sh
@@ -7,6 +7,7 @@ git clone git@gist.github.com:3187620.git gist-3187620
 cd ~/Documents/
 git clone git@github.com:github/hub.git
 git clone git@github.com:git/git.git
+git clone git@github.com:ipython/ipython.git
 
 mkdir -p ~/Documents/Continuum
 cd ~/Documents/Continuum
