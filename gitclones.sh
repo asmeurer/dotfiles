fd5b49e56e52ef99c079bc424e80d49867c3390d
Aaron Meurer <asmeurer@gmail.com>
2014-01-13 12:13:55 -0600

    Add hub and conda to the git clones

--- gitclones.sh
+++ gitclones.sh
@@ -3,3 +3,10 @@
 mkdir -p ~/Documents/gists/
 cd ~/Documents/gists/
 git clone git@gist.github.com:3187620.git gist-3187620
+
+cd ~/Documents/
+git clone git@github.com:github/hub.git
+
+mkdir -p ~/Documents/Continuum
+cd ~/Documents/Continuum
+git clone git@github.com:pydata/conda.git
