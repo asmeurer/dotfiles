8a87ca83ed3d479d5ef292efd63ca1c6aa114dc1
Aaron Meurer <asmeurer@gmail.com>
2014-01-12 20:01:04 -0600

    First (hopefully working) version of linkfiles.py

--- linkfiles.py
+++ linkfiles.py
@@ -21,39 +21,68 @@ glob. An example IGNORE file might look like:
 .*~
 other
 
+TODO:
+
+- Support linking directories
+
 """
 
+from __future__ import print_function
+
 import sys
-import os
 import glob
 import argparse
+
+from os import walk, symlink
+from os.path import join, relpath, abspath, exists
+
+from itertools import chain
 
 def main():
     parser = argparse.ArgumentParser(description="""Link the dotfiles to where
     they're supposed to go.""",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-s', '--source', action='store', default='.', help="""The
-    source of the files to link""")
+    parser.add_argument('-s', '--source', action='store', default='.',
+        help="""The source of the files to link""")
     parser.add_argument('-d', '--destination', action='store', help="""The
-    destination of the files to link""", default='~')
-    parser.add_argument("-i", "--ignorefile", action='store', help="""File containing
-    list of paths to ignore (the file itself and this script are always
-    ignored)""", default="IGNORE")
+        destination of the files to link""", default='~')
+    parser.add_argument("-i", "--ignorefile", action='store', help="""File
+        containing list of paths to ignore (the file itself and this script
+        are always ignored)""", default="IGNORE")
+    parser.add_argument('--dry-run', action='store_true', default=False,
+        help="""Show what would be done without doing it.""")
 
     args = parser.parse_args()
 
-    if os.path.exists(args.ignorefile):
+    if exists(args.ignorefile):
         with open(args.ignorefile) as f:
             ignore = f.read().strip().split()
+        ignore = list(map(abspath, chain(*map(glob.glob, ignore))))
     else:
         ignore = []
         print("No ignore file found")
 
     ignore.append(__file__)
 
-    for dirpath, dirnames, filenames in os.walk(args.source):
-        pass
+    for dirpath, dirnames, filenames in walk(args.source):
+        dest_head = join(args.destination, relpath(dirpath, start=args.source))
+        for file in filenames:
+            fullpath = join(dirpath, file)
+            if any(abspath(fullpath).startswith(i) for i in ignore):
+                continue
+            else:
+                destination = join(dest_head, relpath(join(dirpath, file),
+                    dirpath))
+                if args.dry_run:
+                    print("Would link:", end=' ')
+                else:
+                    print("Linking:", end=' ')
+                print(abspath(fullpath), "to", destination)
+
+                if not args.dry_run:
+                    symlink(abspath(fullpath), destination)
+
 
 
 if __name__ == '__main__':
