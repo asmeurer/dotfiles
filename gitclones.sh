82d8f4f9c9ccc55e2d922d3b4f3ef4224bd8d0f1
Aaron Meurer <asmeurer@gmail.com>
2014-01-14 09:56:30 -0600

    Add all the emacs stuff to gitclones.sh

--- gitclones.sh
+++ gitclones.sh
@@ -10,6 +10,48 @@ git clone git@github.com:git/git.git
 git clone git@github.com:ipython/ipython.git
 git clone git@github.com:mirrors/emacs.git
 
+# Needed in .emacs:
+# TODO: byte compile some stuff
+git clone git@github.com:dacap/keyfreq.git
+git clone git@github.com:cask/cask.git
+cd ~
+cask
+cd ~/Documents/
+git clone git@github.com:winterTTr/ace-jump-mode.git
+git clone git@github.com:nonsequitur/smex.git
+git clone git@github.com:technomancy/ido-ubiquitous.git
+git clone git://jblevins.org/git/markdown-mode.git
+bzr branch lp:mediawiki-el
+git clone git@github.com:yoshiki/yaml-mode.git
+git clone git@github.com:defunkt/coffee-mode.git
+git clone git@github.com:fgallina/python.el.git
+cd python.el
+git remote add github git@github.com:asmeurer/python.el.git
+git fetch github
+git checkout indentation
+cd ~/Documents/
+git clone git@github.com:purcell/mmm-mode.git
+# TODO: doctest-mode
+git clone git@github.com:auto-complete/auto-complete.git
+git clone git@github.com:auto-complete/popup-el.git
+git clone git@github.com:auto-complete/fuzzy-el.git
+git clone git@github.com:kiwanami/emacs-deferred.git
+git clone git@github.com:kiwanami/emacs-ctable.git
+git clone git@github.com:kiwanami/emacs-epc.git
+git clone git@github.com:tkf/emacs-jedi.git
+cd emacs-jedi
+conda create -n env jedi
+cd ~/Documents/
+git clone git@github.com:m2ym/popwin-el.git
+git clone git@github.com:m2ym/direx-el.git
+git clone git@github.com:tkf/emacs-jedi-direx.git
+git clone git@github.com:m2ym/yascroll-el.git
+git clone git@github.com:magnars/expand-region.el.git
+git clone git@github.com:magnars/multiple-cursors.el.git
+git clone http://www.dr-qubit.org/git/undo-tree.git
+git clone git@github.com:kiwanami/emacs-window-layout.git
+git clone git@github.com:kiwanami/emacs-window-manager.git
+
 mkdir -p ~/Documents/Continuum
 cd ~/Documents/Continuum
 git clone git@github.com:pydata/conda.git
