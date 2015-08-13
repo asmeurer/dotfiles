ba3acfb2c1d1533437c3181f647b313b9946c27b
Aaron Meurer <asmeurer@gmail.com>
2015-08-12 22:44:00 -0600

    Better IPython/iTerm2 shell integration
    The integration is now working correctly, but I still need to correctly get
    IPython to count the number of characters in the prompt.

--- .ipython/profile_default/startup/iterm2.py
+++ .ipython/profile_default/startup/iterm2.py
@@ -2,7 +2,7 @@ import sys
 
 try:
     from IPython.core.prompts import LazyEvaluate
-    from iterm2_tools import (BEFORE_PROMPT, AFTER_PROMPT, BEFORE_OUTPUT,
+    from iterm2_tools import (BEFORE_PROMPT, AFTER_PROMPT, before_output,
         after_output)
 
     ipython = get_ipython()
@@ -28,12 +28,31 @@ try:
         return ''
 
     ipython.prompt_manager.lazy_evaluate_fields['before_prompt'] = ipython_before_prompt
-    ipython.prompt_manager.lazy_evaluate_fields['after_prompt'] = ipython_after_prompt
+    ipython.prompt_manager.lazy_evaluate_fields['after_prompt'] = AFTER_PROMPT
     ipython.prompt_manager.lazy_evaluate_fields['before_output'] = ipython_before_output
     ipython.prompt_manager.lazy_evaluate_fields['after_output'] = ipython_after_output
 
-    ipython.prompt_manager.in_template = ("{after_output}" + BEFORE_PROMPT +
-        ipython.prompt_manager.in_template + AFTER_PROMPT)
-    ipython.prompt_manager.out_template = ipython.prompt_manager.out_template + BEFORE_OUTPUT
+    orig_in_template = ipython.prompt_manager.in_template
+    ipython.prompt_manager.in_template = ("\001" + BEFORE_PROMPT + "\002" + ipython.prompt_manager.in_template +
+        "\001" + AFTER_PROMPT + "\002")
+
+    def iterm_pre_prompt_hook(self):
+        after_output(0)
+
+    ipython.set_hook('pre_prompt_hook', iterm_pre_prompt_hook)
+
+    class iTermCallback(object):
+        def __init__(self, ip):
+            self.shell = ip
+
+        def pre_execute(self):
+            before_output()
+
+    def load_ipython_extension(ip):
+        vw = iTermCallback(ip)
+        ip.events.register('pre_execute', vw.pre_execute)
+
+    load_ipython_extension(ipython)
+
 except ImportError:
     raise
