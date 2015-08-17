f0bf2310f783e83ae7ec86f105fd586b0601d398
Aaron Meurer <asmeurer@gmail.com>
2015-08-17 11:31:25 -0600

    Cleanup iTerm2 IPython integration. Add command status support.

--- .ipython/profile_default/startup/iterm2.py
+++ .ipython/profile_default/startup/iterm2.py
@@ -1,5 +1,3 @@
-import sys
-
 try:
     from IPython.core.prompts import LazyEvaluate
     from iterm2_tools import (BEFORE_PROMPT, AFTER_PROMPT, before_output,
@@ -7,52 +5,35 @@ try:
 
     ipython = get_ipython()
 
-    @LazyEvaluate
-    def ipython_before_prompt():
-        before_prompt()
-        return ''
-
-    @LazyEvaluate
-    def ipython_after_prompt():
-        after_prompt()
-        return ''
-
-    @LazyEvaluate
-    def ipython_before_output():
-        before_output()
-        return ''
+    global status
+    status = 0
 
     @LazyEvaluate
     def ipython_after_output():
-        after_output(bool(sys.exc_info()[0]))
+        global status
+        after_output(status)
+        status = 0
         return ''
 
-    ipython.prompt_manager.lazy_evaluate_fields['before_prompt'] = ipython_before_prompt
+    ipython.prompt_manager.lazy_evaluate_fields['before_prompt'] = BEFORE_PROMPT
     ipython.prompt_manager.lazy_evaluate_fields['after_prompt'] = AFTER_PROMPT
-    ipython.prompt_manager.lazy_evaluate_fields['before_output'] = ipython_before_output
     ipython.prompt_manager.lazy_evaluate_fields['after_output'] = ipython_after_output
 
     orig_in_template = ipython.prompt_manager.in_template
-    ipython.prompt_manager.in_template = ("\001" + BEFORE_PROMPT + "\002" + ipython.prompt_manager.in_template +
-        "\001" + AFTER_PROMPT + "\002")
-
-    def iterm_pre_prompt_hook(self):
-        after_output(0)
-
-    ipython.set_hook('pre_prompt_hook', iterm_pre_prompt_hook)
-
-    class iTermCallback(object):
-        def __init__(self, ip):
-            self.shell = ip
-
-        def pre_execute(self):
-            before_output()
+    ipython.prompt_manager.in_template = ("\001{after_output}\002\001{before_prompt}\002" + ipython.prompt_manager.in_template +
+        "\001{after_prompt}\002")
 
     def load_ipython_extension(ip):
-        vw = iTermCallback(ip)
-        ip.events.register('pre_execute', vw.pre_execute)
+        ip.events.register('pre_execute', before_output)
 
     load_ipython_extension(ipython)
 
+    def exc_handler(self, etype, value, tb, tb_offset=None):
+        global status
+        status = 1
+        return self.showtraceback()
+
+    ipython.set_custom_exc((Exception,), exc_handler)
+
 except ImportError:
-    raise
+    pass
