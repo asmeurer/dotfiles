eea5c7d0d0de3c222a3cca827bda00bd54af04f8
Aaron Meurer <asmeurer@gmail.com>
2015-12-01 16:11:24 -0600

    Add git backups command
    Thanks to http://stackoverflow.com/a/5879550/161801 for showing how to do
    this.

--- bin/git-backup.sh
+++ bin/git-backup.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-# This commits any unstaged changes in a git repository, then
-# immideiatly reverts them back to being unstaged.  The idea is that
-# this will backup the changes so that they can be recovered via `git
-# reflog` in case they are accidently removed via `git reset --hard`.
+# This commits any unstaged changes in a git repository, then immediately
+# reverts them back to being unstaged.  The idea is that this will backup the
+# changes so that they can be recovered via `git fsck --unreachable` in case
+# they are accidently removed via `git reset --hard`.
+
+# You can find backup commits with
+
+# git fsck --unreachable | grep commit | cut -d" " -f3 | xargs git log # --merges --no-walk --grep='Backup Commit'
+
+# It is useful to add this as an alias in ~/.gitconfig, like
+
+# [alias]
+#     backup = !git-backup.sh
+#     backups = !git fsck --unreachable | grep commit | cut -d' ' -f3 | xargs git log --merges --no-walk --grep='Backup Commit' --stat
 
 git stash save --keep-index "Backup Commit (WIP) `date "+%Y-%m-%d %H:%M:%S %a"`"
 git stash pop --index
