89bb8086fd19be3a985c17bb2a9d3463e67de325
Aaron Meurer <asmeurer@gmail.com>
2016-06-15 13:08:12 -0400

    Disable autoedit syntax in IPython config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -179,7 +179,7 @@ except ImportError:
 # TerminalInteractiveShell will inherit config from: InteractiveShell
 
 # auto editing of files with syntax errors.
-c.TerminalInteractiveShell.autoedit_syntax = True
+# c.TerminalInteractiveShell.autoedit_syntax = False
 
 # Use colors for displaying information about objects. Because this information
 # is passed through a pager (like 'less'), and some pagers get confused with
