7a655bd12efdc8e172758a0ef43579dd31b7c1b0
Aaron Meurer <asmeurer@gmail.com>
2016-06-28 17:12:10 -0400

    Add Jupyter custom.js to disable auto-closing parentheses

---
+++ .jupyter/custom/custom.js
@@ -0,0 +1,4 @@
+// http://stackoverflow.com/a/33015443/161801
+require(['notebook/js/codecell'], function (codecell) {
+    codecell.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+})
