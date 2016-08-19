f1096ad6479fdd42f258447e4312f8118e070790
Aaron Meurer <asmeurer@gmail.com>
2016-08-19 12:22:38 -0400

    Disable Jedi support in .pythonrc

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,7 +1,8 @@
 # .pythonrc.py
-try:
-    from jedi.utils import setup_readline
-    setup_readline()
-except ImportError:
-    import readline, rlcompleter
-    readline.parse_and_bind("tab: complete")
+
+# try:
+#     from jedi.utils import setup_readline
+#     setup_readline()
+# except ImportError:
+#     import readline, rlcompleter
+#     readline.parse_and_bind("tab: complete")
