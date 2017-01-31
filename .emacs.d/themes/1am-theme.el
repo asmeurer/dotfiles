078305545cf48bff74ec73b9ecd78fd70bc69559
Aaron Meurer <asmeurer@gmail.com>
2017-01-31 02:13:17 -0500

    Add all the font-lock colors to the emacs theme

--- .emacs.d/themes/1am-theme.el
+++ .emacs.d/themes/1am-theme.el
@@ -8,6 +8,12 @@
  '(font-lock-comment-face ((t (:foreground "white"))))
  '(font-lock-doc-face ((t (:inherit font-lock-string-face :foreground
                                     "brightyellow"))))
+ '(font-lock-keyword-face ((t (:foreground "purple"))))
+ '(font-lock-builtin-face ((t (:foreground "dark slate blue"))))
+ '(font-lock-constant-face ((t (:foreground "dark cyan"))))
+ '(font-lock-function-name-face ((t (:foreground "Blue1"))))
+ '(font-lock-type-face ((t (:foreground "ForestGreen"))))
+ '(font-lock-variable-name-face ((t (:foreground "sienna"))))
  '(minibuffer-prompt ((t (:foreground "red"))))
  '(trailing-whitespace ((t (:background "red1"))))
  '(flycheck-error ((t (:inherit flymake-errline :underline t))))
