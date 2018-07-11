605c1ac8f861308eff97448dc59f6fd6e99e578f
Aaron Meurer <asmeurer@gmail.com>
2018-07-11 17:41:24 -0500

    Support X forwarding in the Dockerfile

--- linux-testing/build_and_run.sh
+++ linux-testing/build_and_run.sh
@@ -5,5 +5,8 @@ set -x
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+# allow x forwarding access from localhost
+xhost + 127.0.0.1
+
 docker build -f Dockerfile . -t asmeurer/linux-testing
-docker run -it asmeurer/linux-testing
+docker run -e DISPLAY=docker.for.mac.localhost:0 -it asmeurer/linux-testing
