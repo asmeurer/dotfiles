aa1051bf68836e4da49f79824e890b34de823fb3
Aaron Meurer <asmeurer@gmail.com>
2018-07-12 10:05:49 -0500

    Only use X forwarding with -X

--- linux-testing/build_and_run.sh
+++ linux-testing/build_and_run.sh
@@ -5,8 +5,13 @@ set -x
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-# allow x forwarding access from localhost
-xhost + 127.0.0.1
+while getopts "X" opt; do
+    case "$opt" in
+        X)
+            # allow x forwarding access from localhost
+            xhost + 127.0.0.1
+    esac
+done
 
 docker build -f Dockerfile . -t asmeurer/linux-testing
 docker run -e DISPLAY=docker.for.mac.localhost:0 -it asmeurer/linux-testing
