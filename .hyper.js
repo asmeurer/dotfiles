23c51b129fb819b56b0b558e6ea7351ffdd95aeb
Aaron Meurer <asmeurer@gmail.com>
2019-05-16 16:11:27 -0600

    Add some hyper extensions

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,19 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+
+    // https://hyper.is/plugins?newest
+    plugins: [
+        "hypercwd",
+        "hyper-search",
+        "hyper-fading-scrollbar",
+        "hyper-tabs-enhanced",
+    ],
+
+    hyperTabs: {
+        tabIconsColored: true,
+        trafficButtons: true,
+    },
 
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
