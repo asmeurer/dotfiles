e1bce6af5a421124cc19eaab7e45f0bb29320de7
Aaron Meurer <asmeurer@gmail.com>
2021-09-24 16:54:27 -0600

    Attempts at fixing the highlight color in emacs
    I would like for it to be something better when the highlighted text is hard
    to see. This doesn't seem to work.

--- .emacs.d/themes/1am-theme.el
+++ .emacs.d/themes/1am-theme.el
@@ -7,7 +7,7 @@
  ;; TODO: Make this switch automatically.
  ;; '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#00f900" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "Menlo"))))
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "Menlo"))))
-
+ '(region ((t (:background "lightgoldenrod2" :extend t :distant-foreground "black"))))
  '(highlight-indent-face ((t (:background "color-233"))))
  '(font-lock-string-face ((t (:foreground "red"))))
  '(font-lock-comment-face ((t (:foreground "white"))))
